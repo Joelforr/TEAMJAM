b04c76ae5c8dc587a49e4633a9ff6d0211b8dab4
M. Palladino <Michael21115@yahoo.com>
2016-09-08 15:51:15 -0400

    Initial Commit -- 1st Pass ParallelWorlds

---
+++ ParallelWorld/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float jumpSpeed = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+			transform.position -= new Vector3(speed, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+			transform.position += new Vector3(speed, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)){
+			transform.position += new Vector3(0, jumpSpeed, 0);
+		}
+	}
+}
