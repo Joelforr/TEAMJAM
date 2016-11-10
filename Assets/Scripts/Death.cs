aa3a334fd1f0e7774a8bf238d51636e8f87ad098
M. Palladino <Michael21115@yahoo.com>
2016-11-10 17:18:05 -0500

    Added temp Art to Level 1 - WallClimbLevel

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Debug.Log ("You're Dead, Love " + collider.gameObject.name);
+	}
+}
