ee17e3b566d0187c09426d5265bdc443b83db5c6
Brenner Cheu-Chen <ihave21eyes@gmail.com>
2016-11-29 15:31:28 -0500

    checkpoint code, and unfinalized checkpoint positions

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,11 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour {
 
+    public Scene scene;
+    public GameObject [] CheckPoint;
+     
+    public Vector3 CheckPos;
+        
+    void Start()
+    {
+        CheckPoint = GameObject.FindGameObjectsWithTag("checkpoint");
+    }
+   
+    
 	void OnTriggerEnter2D (Collider2D collider) {
-		//if (collider.gameObject.tag == "DeathZone"){
-			Debug.Log ("You're Dead, Love " + collider.gameObject.name);
-		//}
+		if (collider.gameObject.tag == "DeathZone"){
+            
+            Debug.Log ("You're Dead, Love " + collider.gameObject.name);
+
+            //send to checkpoint
+           transform.position = CheckPos;
+
+            
+        }
+
+        if (collider.gameObject.tag == "checkpoint")
+        {
+           // Debug.Log("check");
+           //set checkpoint
+            CheckPos = collider.bounds.center;
+        }
 	}
 }
